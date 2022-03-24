85cb6c836031e6d7b2e0c15ac69bafaf636ea8a1
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-03-24 13:47:17 +0100

    ex4 started

--- f2/ex4/script.js
+++ f2/ex4/script.js
@@ -1,13 +1,52 @@
 var expect = chai.expect;
+var checkboxLS = document.getElementById("localstorage");
+var txtArea = document.getElementById("txtArea");
+
+const mapDatabase = new Map();
+// localStorage.setItem('key', 'value');
+// localStorage.getItem('key');
+// localStorage.removeItem('key');
 
 function buttonHandler(el) {
-  var textAreaVal = el.parentElement.children[0].value
-  console.log(textAreaVal)
+  var textAreaVal = txtArea.value;
 
+  var input = textAreaVal.split(" ");
+  if (input[0] == "dodaj") {
+    var data = mapDatabase.get(input[1]);
+    if (data == undefined || data.length == 0) {
+      mapDatabase.set(input[1], parseInt(input[2]));
+    } else {
+      data = parseInt(input[2]) + data;
+      mapDatabase.set(input[1], data);
+    }
+
+  } else if (input[0] == "wczytaj") {
+    var data = mapDatabase.get(input[1]);
+    if (data == undefined || data.length == 0) {
+      window.alert("Wpis nie istnieje.");
+    } else {
+      window.alert(data);
+    }
+
+  } else if (input[0] == "usun") {
+    var data = mapDatabase.get(input[1]);
+    if (data == undefined || data.length == 0) {
+      window.alert("Wpis nie istnieje.");
+    } else {
+      mapDatabase.delete(input[1]);
+    }
+    
+  } else if (input[0] == "modyfikuj") {
+    var data = mapDatabase.get(input[1]);
+    if (data == undefined || data.length == 0) {
+      window.alert("Wpis nie istnieje.");
+    } else {
+      mapDatabase.set(input[1], parseInt(input[2]));
+    }
+  }
 }
 
 describe("TEST1", function () {
-  
   it("DESCRIBE", function () {
     expect(4).to.equal(4);
   });
