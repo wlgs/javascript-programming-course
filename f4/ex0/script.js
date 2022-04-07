9c7f2fb66c875ff066d8aacfc81e7477def7bda7
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-04-07 13:55:59 +0200

    f4 ex almost ended

---
+++ f4/ex0/script.js
@@ -0,0 +1,59 @@
+var mainEl = document.getElementsByTagName("main")[0];
+var wordList = [
+  "lorem",
+  "ipsum",
+  "rower",
+  "kanapa",
+  "telewizor",
+  "samolot",
+  "masny",
+  "transkrypt",
+  "laptop",
+  "komputer",
+  "talerz",
+  "podkładka",
+];
+
+var points = 0;
+
+function addPoints(val){
+    points += val;
+    document.getElementById('pkt').textContent = String(points);
+}
+
+function calculatePoints(text){
+    if(parseInt(text)>0)
+        addPoints(parseInt(text));
+    else{
+        addPoints(text.length);
+    }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+document.getElementById("add").onclick = function () {
+  if (getRandomInt(2) == 1) {
+      var spanElement = document.createElement('span')
+      spanElement.textContent = " " + wordList[getRandomInt(wordList.length)];
+      spanElement.addEventListener('click',(ev)=>{
+        calculatePoints(ev.target.textContent);
+        ev.target.remove();
+      })
+      mainEl.children[getRandomInt(6)].appendChild(spanElement);
+  } else {
+    var spanElement = document.createElement('span')
+    spanElement.textContent = " " + String(getRandomInt(wordList.length))
+    spanElement.addEventListener('click',(ev)=>{
+        calculatePoints(ev.target.textContent);
+        ev.target.remove();
+      })
+    mainEl.children[getRandomInt(6)].appendChild(spanElement);
+  }
+};
+
+document.getElementById("del").onclick = function () {
+  var chosen = getRandomInt(6);
+  mainEl.children[chosen].children[getRandomInt(mainEl.children[chosen].children.length)].remove();
+};
